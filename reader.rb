b0ee5e422271d597ac406febe89cefd9868034ef
Bartosz Zawada <BeBouR@Gmail.com>
2012-02-07 23:39:58 +0100

    Added support for YAML output format

--- reader.rb
+++ reader.rb
@@ -16,6 +16,7 @@ def printhelp
     puts "USE: ruby #{$0} [options] PcapFile ", ''
     puts '[options] may be:'
     puts '-j | --json : Output will be in JSON (default)'
+    puts '-y | --yaml : Output will be in YAML'
     puts '-x | --xml : Output will be in XML'
     exit
 end
@@ -32,8 +33,9 @@ mode = config[:default_output]
 # Parse command line arguments
 ARGV.each do |arg|
     if arg[0] == '-'
-        mode = :XML if arg == '--xml' || arg == '-x'
-        mode = :JSON if arg == '--json' || arg == '-j'
+        mode = 'xml' if arg == '--xml' || arg == '-x'
+        mode = 'json' if arg == '--json' || arg == '-j'
+        mode = 'yaml' if arg == '--yaml' || arg == '-y'
     else
         # If it's not an option, it ought to be the pcapfile
         if file == :NO_FILE
@@ -55,16 +57,23 @@ a = []
 capture.each do |packet|
     values = {}
     values[config[:tag_time]] = packet.time_i if config[:read_time]
-    values[config[:tag_size]] = packet.time_i if config[:read_size]
+    values[config[:tag_size]] = packet.size if config[:read_size]
     a << values
 end
 capture.close
 
-a.sort! {|x,y| x[0] <=> y[0]}
+a.sort! {|x,y| x[config[:sort_by]] <=> y[config[:sort_by]]}
 
 if mode == 'json'
     puts JSON.dump({config[:tag_main] => a})
-else
+
+elsif mode == 'yaml'
+    puts YAML.dump({config[:tag_main] => a})
+
+elsif mode == 'xml'
     a.map! {|packet| "  <packet>\n    <ts>#{packet[0]}</ts>\n    <sz>#{packet[1]}</sz>\n  </packet>\n" }
     puts "<data>\n" + a.join("\n") + '</data>'
+
+else
+    puts "Error: Unknown output format: #{config[:default_output]}"
 end
